daf88170bc14348927fe6d64890da82c4ada1f3a
Sarah Young <hi.sarah.young@gmail.com>
2018-11-02 10:18:28 +0100

    Finished GuessedWords component.

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -32,6 +32,27 @@ describe('if there are no words guessed', () => {
 });
 
 describe('if there are words guessed', () => {
-  
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWords }); // Key to have value of guessedWords variable
+  });
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
 
+  test('renders guesed words section', () => {
+    const guessedWordsNode = findByTestAttr(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1);
+  });
+
+  test('correct number of guessed words', () => {
+    const guessedWordNode = findByTestAttr(wrapper, 'guessed-word');
+    expect(guessedWordNode.length).toBe(guessedWords.length);
+  });
 });
