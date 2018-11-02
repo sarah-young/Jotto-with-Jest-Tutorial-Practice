5b53284c319f58ff88be76abc4ff9b3e314d8c89
Sarah Young <hi.sarah.young@gmail.com>
2018-11-02 17:56:16 +0100

    Committing current tests

--- src/input.js
+++ src/input.js
@@ -3,12 +3,35 @@ import { connect } from 'react-redux';
 
 class Input extends Component {
   render() {
-    return <div />
+    const contents = this.props.success //Why am I getting an unexpected token error here?
+    ? null
+    : (
+    <form className="form-inline">
+      <input
+        data-test="input-box"
+        className="mb-2 mx-sm-3"
+        id="word-guess"
+        type="text"
+        placeholder="enter guess" />
+      <button
+        data-test="submit-button"
+        type="submit"
+        className="btn btn-primary mb-2">
+      Submit
+      </button>
+    </form>
+  );
+    return (
+      <div data-test="component-input">
+     { contents }
+    </div>
+    )
   }
 };
 
-const mapStateToProps = (state) => {
+const mapStateToProps = ({success}) => {
   return {};
-}
+};
+
 
 export default connect(mapStateToProps)(Input);
--- src/input.test.js
+++ src/input.test.js
@@ -4,24 +4,30 @@ import { shallow } from 'enzyme';
 import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
 
-const setup = ( initialState={} ) => {
+const setup = (initialState={}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />).dive();
-
-}
-
-
+  return wrapper;
+};
 
 describe('render', () => {
   describe('word has not been guessed', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = {success: false};
+      wrapper = setup(initialState);
+    });
     test('renders without error', () => {
-
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
     test('renders input box', () => {
-
+      const component = findByTestAttr(wrapper, 'input-box');
+      expect(component.length).toBe(1);
     });
     test('renders submit button', () => {
-
+      const component = findByTestAttr(wrapper, 'submit-button');
+      expect(component.length).toBe(1);
     });
   });
   describe('word has been guessed', () => {
