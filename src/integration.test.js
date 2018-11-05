af6e5cc3e3748ddfd1c9b5fa1707db76ae4ef4be
Sarah Young <hi.sarah.young@gmail.com>
2018-11-05 19:08:16 +0100

    Set up guessedWords test

---
+++ src/integration.test.js
@@ -0,0 +1,29 @@
+import { storeFactor } from '../test/testUtils';
+import { guessWord } from './actions'; // guessWord or guessedWord?
+
+describe ('guessWord action dispatcher', () => {
+  describe('no guessed words', ()=> {
+    test('updates state correctly for unsuccessful guessedLetterSet', () => {
+
+    });
+  });
+
+  describe('no guessed words', ()=> {
+    test('updates state correctly for successful guessedLetterSet', () => {
+
+    });
+  });
+
+  describe('some guessed words', ()=> {
+    test('', () => {
+
+    });
+  });
+
+  describe('some guessed words', ()=> {
+    test('', () => {
+
+    });
+  });
+
+});
