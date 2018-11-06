a1404b3c2bb2ad52c09be28cb8e6e52dc3731314
Sarah Young <hi.sarah.young@gmail.com>
2018-11-06 12:51:22 +0100

    Create integration tests

--- test/testUtils.js
+++ test/testUtils.js
@@ -11,12 +11,6 @@ export const storeFactory = (initialState) => {
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`)
 }
-//Importable into all test files
-//Takes in {shallow} wrapper from Enzyme
-//And value for the data-test attribute
-// Uses find method on the shallow wrapper
-// (Find method takes a CSS style selector)
-// ...to find `[data-test=${val}]`
 
 //Generic test type
 export const checkProps = (component, conformingProps) => {
